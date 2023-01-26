734ba54bbd630bc328eefab01906beaca40b0e6b
Jason <82789916+jele4496@users.noreply.github.com>
2023-01-26 14:18:23 -0700

    Update Test.cpp

--- .github/workflows/Test.cpp
+++ .github/workflows/Test.cpp
@@ -1,13 +1,53 @@
 #define CATCH_CONFIG_MAIN
 #include "functions_to_implement.cpp"
 #include <vector>
-#include <cassert>
+#include "main.cpp"
+#include <gtest/gtest.h>
 
-TEST_CASE ("Factorials are computed", "[factorial]"){
-    REQUIRE( Factorial(0) == 0);
-    REQUIRE( Factorial(1) == 1);
-    REQUIRE( Factorial(2) == 2);
-    REQUIRE( Factorial(3) == 6);
-    REQUIRE( Factorial(10) == 3628800);
+TEST(RemoveTwosTest, PositiveNos) { 
+    int num = 12;
+    int expected = 3;
+    int actual = RemoveTwos(num);
+    ASSERT_EQ(expected, actual);
+}
+
+TEST(RemoveTwosTest, NegativeNos) { 
+    int num = -12;
+    int expected = -3;
+    int actual = RemoveTwos(num);
+    ASSERT_EQ(expected, actual);
+}
+
+TEST(FactorialTest, PositiveNos) { 
+    long long num = 5;
+    long long expected = 120;
+    long long actual = Factorial(num);
+    ASSERT_EQ(expected, actual);
+}
+
+TEST(SignTest, PositiveNos) { 
+    int num = 5;
+    int expected = 1;
+    int actual = Sign(num);
+    ASSERT_EQ(expected, actual);
+}
+
+TEST(SignTest, NegativeNos) { 
+    int num = -5;
+    int expected = -1;
+    int actual = Sign(num);
+    ASSERT_EQ(expected, actual);
+}
+
+TEST(SignTest, Zero) { 
+    int num = 0;
+    int expected = 0;
+    int actual = Sign(num);
+    ASSERT_EQ(expected, actual);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
 
